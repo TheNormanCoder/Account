3009c0f5f83a5747923f8b4f6af9d446fe7f654e
TheNormanCoder <n.billanova@gmail.com>
2023-05-02 17:26:35 +0200

    Update AccountControllerTest.java

--- src/test/java/com/example/account/AccountControllerTest.java
+++ src/test/java/com/example/account/AccountControllerTest.java
@@ -1,101 +1,79 @@
 package com.example.account;
 
-import com.example.account.service.balance.BalanceService;
 import com.example.account.service.balance.response.BalanceResponse;
-import com.example.account.service.payment.PaymentService;
 import com.example.account.service.payment.request.Payment;
 import com.example.account.service.payment.response.MoneyTransfer;
-import com.example.account.service.transaction.TransactionService;
 import com.example.account.service.transaction.response.TransactionResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
 
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.Mockito.when;
 
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.time.LocalDate;
-
-@ExtendWith(SpringExtension.class)
-@ExtendWith(MockitoExtension.class)
 @WebMvcTest(AccountController.class)
 public class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @InjectMocks
+    @MockBean
     private AccountController accountController;
 
-    @Mock
-    private BalanceService balanceService;
+    private BalanceResponse balanceResponse;
+    private MoneyTransfer moneyTransfer;
+    private TransactionResponse transactionResponse;
 
-    @Mock
-    private TransactionService transactionService;
+    @BeforeEach
+    public void setUp() {
+        balanceResponse = new BalanceResponse();
+        balanceResponse.setDate("2023-05-02");
+        balanceResponse.setBalance(new BigDecimal(1000));
+        balanceResponse.setAvailableBalance(new BigDecimal(900));
+        balanceResponse.setCurrency("EUR");
 
-    @Mock
-    private PaymentService paymentService;
+        moneyTransfer = new MoneyTransfer();
+        moneyTransfer.setSender("12345678");
+        moneyTransfer.setRecipient("23456789");
+        moneyTransfer.setAmount(new BigDecimal(100));
+        moneyTransfer.setCurrency("EUR");
+
+        transactionResponse = new TransactionResponse();
+        transactionResponse.setAccountId("12345678");
+        transactionResponse.setFromAccountingDate("2023-04-01");
+        transactionResponse.setToAccountingDate("2023-04-30");
+    }
 
     @Test
-    public void testGetBalance() throws Exception {
-        // Arrange
-        String accountId = "12345678";
-        BalanceResponse balanceResponse = new BalanceResponse();
-        balanceResponse.setAccountId(accountId);
-        balanceResponse.setBalance(500.0);
-        when(balanceService.getBalance(accountId)).thenReturn(ResponseEntity.ok(balanceResponse));
-
-        // Act & Assert
-        mockMvc.perform(get("/api/balance/{accountId}", accountId)
-                .contentType(MediaType.APPLICATION_JSON))
+    public void getBalanceTest() throws Exception {
+        when(accountController.getBalance("12345678")).thenReturn(ResponseEntity.ok(balanceResponse));
+        mockMvc.perform(get("/api/balance/12345678"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void testMoneyTransfer() throws Exception {
-        // Arrange
-        String accountId = "12345678";
+    public void moneyTransferTest() throws Exception {
         Payment payment = new Payment();
-        payment.setRecipient("87654321");
-        payment.setAmount(100.0);
-        MoneyTransfer moneyTransfer = new MoneyTransfer();
-        moneyTransfer.setSender(accountId);
-        moneyTransfer.setRecipient("87654321");
-        moneyTransfer.setAmount(100.0);
-        when(paymentService.executePayment(accountId, payment)).thenReturn(ResponseEntity.ok(moneyTransfer));
+        payment.setRecipient("23456789");
+        payment.setAmount(new BigDecimal(100));
 
-        // Act & Assert
-        mockMvc.perform(post("/api/moneyTransfer/{accountId}", accountId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"recipient\":\"87654321\",\"amount\":100.0}"))
+        when(accountController.moneyTransfer("12345678", payment)).thenReturn(ResponseEntity.ok(moneyTransfer));
+        mockMvc.perform(post("/api/moneyTransfer/12345678"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void testGetTransactions() throws Exception {
-        // Arrange
-        String accountId = "12345678";
-        String fromDate = LocalDate.now().minusDays(10).toString();
-        String toDate = LocalDate.now().toString();
-        TransactionResponse transactionResponse = new TransactionResponse();
-        transactionResponse.setAccountId(accountId);
-        transactionResponse.setFromAccountingDate(fromDate);
-        transactionResponse.setToAccountingDate(toDate);
-        when(transactionService.getTransactions(accountId, fromDate, toDate)).thenReturn(ResponseEntity.ok(transactionResponse));
-
-        // Act & Assert
-        mockMvc.perform(get("/api/transactions/{accountId}/{fromAccountingDate}/{toAccountingDate}", accountId, fromDate, toDate)
-                .contentType(MediaType.APPLICATION_JSON))
+    public void getTransactionsTest() throws Exception {
+        when(accountController.getTransactions("12345678", "2023-04-01", "2023-04-30")).thenReturn(ResponseEntity.ok(transactionResponse));
+        mockMvc.perform(get("/api/transactions/12345678/2023-04-01/2023-04-30"))
                 .andExpect(status().isOk());
     }
 }
